7a61b7a72190386a7ddf6bd74dc553858e088b8d
Floris <floris.m.boers@gmail.com>
2023-02-20 22:36:26 +0100

    betere website versie gemaakt

--- PythonCode/server.py
+++ PythonCode/server.py
@@ -17,51 +17,74 @@ app = flask.Flask('__name__')
 
 lichtServoPort = 12
 
+autoslot = "autoslotaan"
+autolicht = "autolichtaan"
 
-
-@app.route('/LichtUit')
+@app.route('/lichtuit')
 def LichtUit():
 
     return flask.redirect('/')
 
-@app.route('/LichtAan')
+@app.route('/lichtaan')
 def LichtAan():
     return flask.redirect('/')
 
-@app.route('/DeurOpen')
+@app.route('/slotopen')
 def DeurOpen():
     return flask.redirect('/')
 
-@app.route('/DeurDicht')
+@app.route('/slotdicht')
 def DeurDicht():
     return flask.redirect('/')
 
-@app.route('/StopContactUit')
+@app.route('/stopcontactuit')
 def StopContactUit():
     return flask.redirect('/')
 
-@app.route('/StopContactAan')
+@app.route('/stopcontactaan')
 def StopContactAan():
     return flask.redirect('/')
 
-@app.route('/SensorUit')
-def SesnorUit():
+
+@app.route('/autolicht')
+def autolichtToggle():
+    global autolicht
+    if autolicht == "autolichtaan":
+        autolicht = "autolichtuit"
+    elif autolicht == "autolichtuit":
+        autolicht = "autolichtaan"
+    print(autolicht)
 
     return flask.redirect('/')
 
-@app.route('/SensorAan')
-def SensorAan():
-    print("sensor aan")
-    #addToTerminal("<br> gay")
+
+@app.route('/autoslot')
+def autoSlotToggle():
+    global autoslot
+    if autoslot == "autoslotaan":
+        autoslot = "autoslotuit"
+    elif autoslot == "autoslotuit":
+        autoslot = "autoslotaan"
+    print(autoslot)
     return flask.redirect('/')
+
+
+licht = "lichtaan"
+slot = "slotdicht"
+temp = 31.5
 
 @app.route('/')
 def home():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html',
+                                  temp=temp,
+                                 slot=slot,
+                                 licht=licht,
+                                 autolicht=autolicht,
+                                 autoslot=autoslot
+                                 )
 
 def maakServer():
     app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     maakServer()
-    blab
